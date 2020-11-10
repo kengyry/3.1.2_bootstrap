bb5b36a55c467af29f228abba787fa9aeb15c10b
kengy <kengyry.ken@yandex.ru>
2020-11-10 14:38:43 +0500

    commit1

---
+++ src/main/java/web/controller/UserController.java
@@ -0,0 +1,116 @@
+package web.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import web.Model.Role;
+import web.Model.User;
+import web.service.RoleService;
+import web.service.UserService;
+
+import java.security.Principal;
+import java.util.HashSet;
+import java.util.Set;
+
+
+@Controller
+public class UserController {
+
+    private final UserService userService;
+    private final RoleService roleService;
+
+    @Autowired
+    public UserController(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
+    @GetMapping("admin")
+    public ModelAndView adminPage(Principal principal) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("admin.html");
+        modelAndView.addObject("allUsers", userService.findAll());
+        modelAndView.addObject("allRoles", roleService.findAllRoles());
+        modelAndView.addObject("currentUser", userService.findByLastName(principal.getName()));
+        modelAndView.addObject("newUser", new User());
+        return modelAndView;
+    }
+
+    @GetMapping("user")
+    public ModelAndView userPage(Principal principal) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("user.html");
+        modelAndView.addObject("allUsers", userService.findAll());
+        modelAndView.addObject("allRoles", roleService.findAllRoles());
+        modelAndView.addObject("currentUser", userService.findByLastName(principal.getName()));
+        return modelAndView;
+    }
+
+    @PostMapping("user-update/{id}")
+    public ModelAndView updateUser(Long id, User user, @RequestParam(value = "roles", required = false) Set<Long> roleIds) {
+        ModelAndView modelAndView = new ModelAndView();
+        Set<Role> currentRoles = userService.findById(id).get().getRoles();
+        Set<Role> roles = new HashSet<>();
+        if (roleIds != null) {
+            for (Long i : roleIds) {
+                roles.add(roleService.findById(i).get());
+                user.setRoles(roles);
+            }
+        } else {
+            user.setRoles(currentRoles);
+        }
+        userService.saveUser(user);
+
+        modelAndView.setViewName("redirect:/admin");
+        return modelAndView;
+    }
+
+    @PostMapping("user-delete/{id}")
+    public ModelAndView deleteUser(@PathVariable("id") Long id) {
+        ModelAndView modelAndView = new ModelAndView();
+        userService.deleteById(id);
+        modelAndView.setViewName("redirect:/admin");
+        return modelAndView;
+    }
+
+    @PostMapping("user-create")
+    public ModelAndView createUser(User user) {
+        ModelAndView modelAndView = new ModelAndView();
+        userService.saveUser(user);
+        modelAndView.setViewName("redirect:/admin");
+        return modelAndView;
+    }
+}
+
+
+
+   /* @GetMapping(value = "user/user-list_")
+    public ModelAndView findAllUser(Principal currentUser) {
+        User user = userService.findByLastName(currentUser.getName());
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("user/user-list_");
+        modelAndView.addObject("users", user);
+        return modelAndView;
+    }
+
+    @GetMapping("admin/user-create")
+    public ModelAndView createUserForm(User user) {
+        ModelAndView modelAndView = new ModelAndView();
+        Set<Role> uniqueRoles = roleService.findAllRoles();
+        modelAndView.addObject("users", user);
+        modelAndView.addObject("roles", uniqueRoles);
+        modelAndView.setViewName("admin/user-create");
+        return modelAndView;
+    }
+
+
+
+
+
+    @GetMapping("admin/user-update/{id}")
+    public ModelAndView updateUserForm(@PathVariable("id") Long id) {
+        ModelAndView modelAndView = new ModelAndView("redirect:/admin/user-list");
+        modelAndView.addObject("users", userService.findById(id));
+        modelAndView.addObject("roles", roleService.findAllRoles());
+        return modelAndView;
+    }*/
